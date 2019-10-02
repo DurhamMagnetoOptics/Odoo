27c3d8d9417470076da07ef220472fa22cc6c478
Pete Seem <p.seem@durhammagnetooptics.com>
2019-10-02 16:46:35 +0100

    working settings.json?

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -5,13 +5,12 @@ from odoo import models, fields, api
 class Course(models.Model):
     _name = 'openacademy.course'
     _description = "OpenAcademy Courses"
-    
+
     name = fields.Char(string="Title", required=True)
+    
     description = fields.Text()
-    
     responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
-    
-	
+
 class Session(models.Model):
     _name = 'openacademy.session'
     _description = "OpenAcademy Sessions"
