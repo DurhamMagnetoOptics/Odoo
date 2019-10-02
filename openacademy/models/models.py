f0b744587a9d2fa9ff9a5a43e2c5a36a76246861
Pete Seem <p.seem@durhammagnetooptics.com>
2019-10-02 14:00:17 +0100

    wrong capitalization

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -9,7 +9,7 @@ class Course(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
     
-    responsible_id = fields.Many2One('res.users', ondelete='set null', string="Responsible", index=True)
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
     
 	
 class Session(models.Model):
@@ -21,8 +21,8 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     
-    instructor_id= fields.Many2One('res.partner', string="Instructor")
-    course_id = fields.Many2One('openacademy.course', ondelete='cascade', string="Course", required=True)
+    instructor_id= fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
 	
 # class openacademy(models.Model):
 #     _name = 'openacademy.openacademy'
