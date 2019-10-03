92f96bffc92927d5c90a0b3c830ccbed689382dc
Pete Seem <p.seem@durhammagnetooptics.com>
2019-10-03 15:20:15 +0100

    Added menu and access rights (p.83)

--- library_app/__manifest__.py
+++ library_app/__manifest__.py
@@ -4,4 +4,8 @@
     'author': 'Daniel Reis',
     'depends': ['base'],
     'application': True,
+    'data': [
+        'security/library_security.xml',
+        'views/library_menu.xml',
+        ]
 }
