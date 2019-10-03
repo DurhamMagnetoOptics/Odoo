f7f85b2e1e0e5c4d823323932937d63c7148a503
Pete Seem <p.seem@durhammagnetooptics.com>
2019-10-03 14:34:32 +0100

    Minimal architectecture for new moduole p.78

---
+++ library_app/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Library Managment',
+    'description': 'Manage library book catalogue and lending.',
+    'author': 'Daniel Reis',
+    'depends': ['base'],
+    'application': True,
+}
