9242d2dd6fcef8962a7fc79bf5351d3dab25850f
Pete Seem <p.seem@durhammagnetooptics.com>
2019-10-04 14:37:48 +0100

    added 10-digit ISBN and test case

---
+++ library_member/tests/test_book.py
@@ -0,0 +1,15 @@
+class TestBook(TransactionCase):
+    def setUp(self, *args, **kwargs):
+        result = super().setUp(*args, **kwargs)
+        user_admin = self.env.ref('base.user_admin')
+        self.env = self.env(user=user_admin)
+        self.Book = self.env['library.book']
+        self.book_ode = self.Book.create({
+            'name': 'Lord of the Flies',
+            'isbn': '0-571-05686-5'
+        })
+        return result
+    
+    def test_check_isbn(self):
+        "Check valid ISBN"
+        self.assertTrue(self.book_ode._check_isbn)
