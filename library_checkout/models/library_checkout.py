1535c2952f35a55e4e072108ee82e9211596e464
pseem <p.seem@durhammagnetooptics.com>
2019-10-07 16:21:40 +0000

    Typos

--- library_checkout/models/library_checkout.py
+++ library_checkout/models/library_checkout.py
@@ -1,11 +1,11 @@
 from odoo import api,  exceptions, fields, models
 
-class Checkout(model.Model):
+class Checkout(models.Model):
     _name='library.checkout'
     _description='Checkout Request'
     member_id = fields.Many2one('library.member', required=True)
     user_id=fields.Many2one('res.users', 'Librarian', default=lambda s: s.env.uid)
-    request_date = fields.Dat(default = lambda s: fields.Date.today())
+    request_date = fields.Date(default = lambda s: fields.Date.today())
     line_ids = fields.One2many('library.checkout.line', 'checkout_id', string='Borrowed Books',)
 
 
