c8b24bfecefaa7f93f543e406a82d450eae44b0b
Pete Seem <p.seem@durhammagnetooptics.com>
2019-10-08 12:40:20 +0100

    Added mass message wizard (p.223)

--- library_checkout/models/library_checkout.py
+++ library_checkout/models/library_checkout.py
@@ -3,6 +3,7 @@ from odoo import api,  exceptions, fields, models
 class Checkout(models.Model):
     _name='library.checkout'
     _description='Checkout Request'
+    _inherit = ['mail.thread', 'mail.activity']
     member_id = fields.Many2one('library.member', required=True)
     user_id=fields.Many2one('res.users', 'Librarian', default=lambda s: s.env.uid)
     request_date = fields.Date(default = lambda s: fields.Date.today())
