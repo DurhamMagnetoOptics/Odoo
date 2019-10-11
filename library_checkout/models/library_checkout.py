87bcacdcd77800e49d349a5429b65a2e6fc3b278
Pete Seem <p.seem@durhammagnetooptics.com>
2019-10-11 22:33:49 +0100

    added graph, pivot, calendar, and activity view

--- library_checkout/models/library_checkout.py
+++ library_checkout/models/library_checkout.py
@@ -26,7 +26,7 @@ class Checkout(models.Model):
         return names
 
 
-    num_books = fields.Integer(compute='_compute_num_books')
+    num_books = fields.Integer(compute='_compute_num_books', store=True)
 
     @api.depends('line_ids')
     def _compute_num_books(self):
