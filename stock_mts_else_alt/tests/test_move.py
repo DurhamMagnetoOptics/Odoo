83acae327fc8cd394e9681ca63b913a0ae65d087
Pete Seem <p.seem@durhammagnetooptics.com>
2019-11-14 21:57:23 +0000

    Test coverage written for all in stock, none in stock, and partial in stock execution of mts_else_alt rule

--- stock_mts_else_alt/tests/test_move.py
+++ stock_mts_else_alt/tests/test_move.py
@@ -3,7 +3,7 @@ from odoo.tests.common import SavepointCase
 class TestMove(SavepointCase):
     def setUp(self, *args, **kwargs):
         result = super().setUp(*args, **kwargs)
-        
+
         self.scheduler = self.env['stock.scheduler.compute']
         self.warehouse = self.env.ref('stock.warehouse0')
         self.uom_xid = self.ref('uom.product_uom_unit')
@@ -148,6 +148,30 @@ class TestMove(SavepointCase):
             'location_out_id': self.Vertical1.id,            
         })         
 
+
+        return result
+    
+    def test_split(self):
+        "Trigger the restock with some but not enough"
+
+        #create inventory
+        #self.inventoryNeed = self.env['stock.inventory'].create({
+        #    'name': 'Fake manufacturing need'      
+        #})      
+        #self.inventoryLineNeed = self.env['stock.inventory.line'].create({
+        #    'product_id': self.CompA.id,
+        #    'product_uom_id': self.uom_xid,
+        #    'inventory_id': self.inventoryNeed.id,
+        #    'product_qty': -4.0,
+        #    'location_id': self.HUST.id,
+        #})
+        #self.inventoryNeed._action_start()
+        #self.inventoryNeed.action_validate()   
+        #self.scheduler._procure_calculation_orderpoint()  #Once to trigger the pull from """
+
+        inStockQty =  2.0
+        neededQty = 5.0
+
         #create inventory
         self.inventoryCompA = self.env['stock.inventory'].create({
             'name': 'Starting CompA Inventory'   
@@ -156,34 +180,154 @@ class TestMove(SavepointCase):
             'product_id': self.CompA.id,
             'product_uom_id': self.uom_xid,
             'inventory_id': self.inventoryCompA.id,
-            'product_qty': 2.0,
+            'product_qty': inStockQty,
             'location_id': self.Vertical1.id,
         })
         self.inventoryCompA._action_start()
         self.inventoryCompA.action_validate()
 
 
-        return result
-    
-    def test_create(self):
-        "Trigger the restock rule in HUST"
+        #Create procurement
+        values = {
+            'warehouse_id': self.warehouse,
+            'route_ids': self.ComponentResupply,
+        }
+        PG = self.env['procurement.group']
+        strOrigin = 'mts_else_mto Test Replenishment'
+        
+        proc = PG.Procurement(
+            self.CompA,
+            neededQty,
+            self.CompA.uom_id,
+            self.HUST,  # Location
+            "Test Replenishment",  # Name
+            strOrigin,  # Origin
+            self.warehouse.company_id,
+            values  # Values
+        )
+        PG.run([proc])
+        
+        myMoves = self.env['stock.move'].search([('origin', '=', strOrigin)])
+        move1 = myMoves.search([('location_id', '=', self.Stores.id), ('location_dest_id', '=', self.HUST.id)])
+        move2 = myMoves.search([('location_id', '=', self.goodsIn.id), ('location_dest_id', '=', self.HUST.id)])
+        move3 = myMoves.search([('location_dest_id', '=', self.goodsIn.id)])
+
+        self.assertEqual(len(myMoves), 3, msg='More than 3 moves generated')
+        self.assertEqual(len(move1), 1, msg='More than 1 move from Stores to HUST')
+        self.assertEqual(len(move2), 1, msg='More than 1 move from Goods In to HUST')
+        self.assertEqual(len(move3), 1, msg='More than 1 move to Goods In')
+
+        self.assertEqual(move1.product_qty, inStockQty, msg='More than %s units in Stores->Hust' % inStockQty)
+        self.assertEqual(move2.product_qty, neededQty - inStockQty, msg='More than %s units in GoodsIn->Hust' % (neededQty - inStockQty))
+        self.assertEqual(move3.product_qty, neededQty - inStockQty, msg='More than %s units in Vendors->GoodsIn' % (neededQty - inStockQty))
+
+    def test_allstock(self):
+        "Trigger the restock with more than enough"
+
+        inStockQty =  8.0
+        neededQty = 5.0
 
         #create inventory
-        self.inventoryNeed = self.env['stock.inventory'].create({
-            'name': 'Fake manufacturing need'      
+        self.inventoryCompA = self.env['stock.inventory'].create({
+            'name': 'Starting CompA Inventory'   
         })      
-        self.inventoryLineNeed = self.env['stock.inventory.line'].create({
+        self.inventoryLineCompA = self.env['stock.inventory.line'].create({
             'product_id': self.CompA.id,
             'product_uom_id': self.uom_xid,
-            'inventory_id': self.inventoryNeed.id,
-            'product_qty': -4.0,
-            'location_id': self.HUST.id,
+            'inventory_id': self.inventoryCompA.id,
+            'product_qty': inStockQty,
+            'location_id': self.Vertical1.id,
         })
-        self.inventoryNeed._action_start()
-        self.inventoryNeed.action_validate()   
+        self.inventoryCompA._action_start()
+        self.inventoryCompA.action_validate()
 
-        self.scheduler._procure_calculation_orderpoint()  #Once to trigger the pull from
+
+        #Create procurement
+        values = {
+            'warehouse_id': self.warehouse,
+            'route_ids': self.ComponentResupply,
+        }
+        PG = self.env['procurement.group']
+        strOrigin = 'mts_else_mto Test Replenishment'
         
-        ##TODO: How do I test for the existence of two new stock moves???
-        self.assertEqual(True, True)
+        proc = PG.Procurement(
+            self.CompA,
+            neededQty,
+            self.CompA.uom_id,
+            self.HUST,  # Location
+            "Test Replenishment",  # Name
+            strOrigin,  # Origin
+            self.warehouse.company_id,
+            values  # Values
+        )
+        PG.run([proc])
+        
+        myMoves = self.env['stock.move'].search([('origin', '=', strOrigin)])
+        move1 = myMoves.search([('location_id', '=', self.Stores.id), ('location_dest_id', '=', self.HUST.id)])
+        #move2 = myMoves.search([('location_id', '=', self.goodsIn.id), ('location_dest_id', '=', self.HUST.id)])
+        #move3 = myMoves.search([('location_dest_id', '=', self.goodsIn.id)])
 
+        self.assertEqual(len(myMoves), 1, msg='More than 3 moves generated')
+        self.assertEqual(len(move1), 1, msg='More than 1 move from Stores to HUST')
+        #self.assertEqual(len(move2), 1, msg='More than 1 move from Goods In to HUST')
+        #self.assertEqual(len(move3), 1, msg='More than 1 move to Goods In')
+
+        self.assertEqual(move1.product_qty, neededQty, msg='More than %s units in Stores->Hust' % neededQty)
+        #self.assertEqual(move2.product_qty, neededQty - inStockQty, msg='More than %s units in GoodsIn->Hust' % (neededQty - inStockQty))
+        #self.assertEqual(move3.product_qty, neededQty - inStockQty, msg='More than %s units in Vendors->GoodsIn' % (neededQty - inStockQty))        
+
+
+    def test_nostock(self):
+        "Trigger the restock with more than enough"
+
+        inStockQty =  0.0
+        neededQty = 5.0
+
+        #create inventory
+        self.inventoryCompA = self.env['stock.inventory'].create({
+            'name': 'Starting CompA Inventory'   
+        })      
+        self.inventoryLineCompA = self.env['stock.inventory.line'].create({
+            'product_id': self.CompA.id,
+            'product_uom_id': self.uom_xid,
+            'inventory_id': self.inventoryCompA.id,
+            'product_qty': inStockQty,
+            'location_id': self.Vertical1.id,
+        })
+        self.inventoryCompA._action_start()
+        self.inventoryCompA.action_validate()
+
+
+        #Create procurement
+        values = {
+            'warehouse_id': self.warehouse,
+            'route_ids': self.ComponentResupply,
+        }
+        PG = self.env['procurement.group']
+        strOrigin = 'mts_else_mto Test Replenishment'
+        
+        proc = PG.Procurement(
+            self.CompA,
+            neededQty,
+            self.CompA.uom_id,
+            self.HUST,  # Location
+            "Test Replenishment",  # Name
+            strOrigin,  # Origin
+            self.warehouse.company_id,
+            values  # Values
+        )
+        PG.run([proc])
+        
+        myMoves = self.env['stock.move'].search([('origin', '=', strOrigin)])
+        #move1 = myMoves.search([('location_id', '=', self.Stores.id), ('location_dest_id', '=', self.HUST.id)])
+        move2 = myMoves.search([('location_id', '=', self.goodsIn.id), ('location_dest_id', '=', self.HUST.id)])
+        move3 = myMoves.search([('location_dest_id', '=', self.goodsIn.id)])
+
+        self.assertEqual(len(myMoves), 2, msg='More than 3 moves generated')
+        #self.assertEqual(len(move1), 1, msg='More than 1 move from Stores to HUST')
+        self.assertEqual(len(move2), 1, msg='More than 1 move from Goods In to HUST')
+        self.assertEqual(len(move3), 1, msg='More than 1 move to Goods In')
+
+        #self.assertEqual(move1.product_qty, neededQty, msg='More than %s units in Stores->Hust' % neededQty)
+        self.assertEqual(move2.product_qty, neededQty, msg='More than %s units in GoodsIn->Hust' % neededQty)
+        self.assertEqual(move3.product_qty, neededQty, msg='More than %s units in Vendors->GoodsIn' % neededQty)
