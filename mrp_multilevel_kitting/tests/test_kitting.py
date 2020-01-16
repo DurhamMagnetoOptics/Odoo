d0d1bd5166a79eb617d8b5ff83d9dd9f965ef933
Pete Seem <p.seem@durhammagnetooptics.com>
2020-01-16 09:24:34 +0000

    Test coverage complete

--- mrp_multilevel_kitting/tests/test_kitting.py
+++ mrp_multilevel_kitting/tests/test_kitting.py
@@ -179,4 +179,205 @@ class TestMove(SavepointCase):
         move2 = myMoves.search([('location_id', '=', self.HUST.id), ('product_id', '=', self.CompB.id)])
 
         self.assertEqual(len(move1), 1, msg='Expected move of CompA not found')
-        self.assertEqual(len(move2), 1, msg='Expected move of CompB not found')                
+        self.assertEqual(len(move2), 1, msg='Expected move of CompB not found')             
+
+    def test_samebinexist(self):
+        "Make sure multilevel_kitting off creates the need in-situ as normal"
+
+        self.warehouse.manu_type_id.multilevel_kitting = True
+        self.MW3_BOM.multilevel_kitting = True
+        self.compA_BOM_line.multilevel_kitting_name = "TestBin"
+        self.compB_BOM_line.multilevel_kitting_name = "TestBin"
+        testBin = self.Location.create({
+            'name': 'TestBin',
+            'location_id': self.HUST.id
+        })  
+
+        #create MO
+        strOrigin = 'mrp_multilevel_kitting Test Replenishment'
+        self.MW3_MO = self.env['mrp.production'].create({
+            'name': strOrigin,
+            'origin': strOrigin,
+            'product_tmpl_id': self.MW3.product_tmpl_id.id,
+            'product_id': self.MW3.id,
+            'product_qty': 1.0,
+            'location_src_id': self.HUST.id,
+            'location_dest_id': self.HUST.id,
+            'bom_id': self.MW3_BOM.id,
+            'product_uom_id': self.ref('uom.product_uom_unit')
+        })              
+
+        self.MW3_MO._onchange_move_raw()
+        self.MW3_MO.action_confirm()
+
+        myMoves = self.env['stock.move'].search([('origin', '=', strOrigin)])
+        move1 = myMoves.search([('location_id', '=', testBin.id), ('product_id', '=', self.CompA.id)])
+        move2 = myMoves.search([('location_id', '=', testBin.id), ('product_id', '=', self.CompB.id)])
+
+        self.assertEqual(len(move1), 1, msg='Expected move of CompA not found')
+        self.assertEqual(len(move2), 1, msg='Expected move of CompB not found')           
+
+    def test_samebincreated(self):
+        "Make sure multilevel_kitting off creates the need in-situ as normal"
+
+        self.warehouse.manu_type_id.multilevel_kitting = True
+        self.MW3_BOM.multilevel_kitting = True
+        self.compA_BOM_line.multilevel_kitting_name = "TestBin"
+        self.compB_BOM_line.multilevel_kitting_name = "TestBin"
+
+
+        #create MO
+        strOrigin = 'mrp_multilevel_kitting Test Replenishment'
+        self.MW3_MO = self.env['mrp.production'].create({
+            'name': strOrigin,
+            'origin': strOrigin,
+            'product_tmpl_id': self.MW3.product_tmpl_id.id,
+            'product_id': self.MW3.id,
+            'product_qty': 1.0,
+            'location_src_id': self.HUST.id,
+            'location_dest_id': self.HUST.id,
+            'bom_id': self.MW3_BOM.id,
+            'product_uom_id': self.ref('uom.product_uom_unit')
+        })              
+
+        self.MW3_MO._onchange_move_raw()
+        self.MW3_MO.action_confirm()
+
+        myMoves = self.env['stock.move'].search([('origin', '=', strOrigin)])
+        move1 = myMoves.search([('product_id', '=', self.CompA.id)])
+        move2 = myMoves.search([('product_id', '=', self.CompB.id)])
+
+        self.assertEqual(len(move1), 1, msg='Expected move of CompA not found')
+        self.assertEqual(len(move2), 1, msg='Expected move of CompB not found')  
+
+        self.assertEqual(move1.location_id.location_id.id, self.HUST.id, msg='CompA not drawn from child of source')          
+        self.assertEqual(move1.location_id.id, move2.location_id.id, msg='Different bins for compA and compB')     
+
+        self.assertEqual(move1.location_id.name, "TestBin", msg='CompA not drawn from child of source')
+
+    def test_diffbins(self):
+        "Make sure multilevel_kitting off creates the need in-situ as normal"
+
+        self.warehouse.manu_type_id.multilevel_kitting = True
+        self.MW3_BOM.multilevel_kitting = True
+        self.compA_BOM_line.multilevel_kitting_name = "TestBin1"
+        self.compB_BOM_line.multilevel_kitting_name = "TestBin2"
+
+        testBin1 = self.Location.create({
+            'name': 'TestBin1',
+            'location_id': self.HUST.id
+        })  
+
+
+        #create MO
+        strOrigin = 'mrp_multilevel_kitting Test Replenishment'
+        self.MW3_MO = self.env['mrp.production'].create({
+            'name': strOrigin,
+            'origin': strOrigin,
+            'product_tmpl_id': self.MW3.product_tmpl_id.id,
+            'product_id': self.MW3.id,
+            'product_qty': 1.0,
+            'location_src_id': self.HUST.id,
+            'location_dest_id': self.HUST.id,
+            'bom_id': self.MW3_BOM.id,
+            'product_uom_id': self.ref('uom.product_uom_unit')
+        })              
+
+        self.MW3_MO._onchange_move_raw()
+        self.MW3_MO.action_confirm()
+
+        myMoves = self.env['stock.move'].search([('origin', '=', strOrigin)])
+        move1 = myMoves.search([('product_id', '=', self.CompA.id)])
+        move2 = myMoves.search([('product_id', '=', self.CompB.id)])
+
+        self.assertEqual(len(move1), 1, msg='Expected move of CompA not found')
+        self.assertEqual(len(move2), 1, msg='Expected move of CompB not found')  
+
+        self.assertEqual(move1.location_id.id, testBin1.id, msg='CompA not drawng from existing location') 
+        
+        self.assertEqual(move2.location_id.name, "TestBin2", msg='CompB location incorrect name')        
+        self.assertEqual(move2.location_id.location_id.id, self.HUST.id, msg='CompB not drawn from child of source')              
+
+    def test_diffbinsoneblank(self):
+        "Make sure multilevel_kitting off creates the need in-situ as normal"
+
+        self.warehouse.manu_type_id.multilevel_kitting = True
+        self.MW3_BOM.multilevel_kitting = True
+        self.compA_BOM_line.multilevel_kitting_name = "TestBin1"
+        self.compB_BOM_line.multilevel_kitting_name = ''
+
+        testBin1 = self.Location.create({
+            'name': 'TestBin1',
+            'location_id': self.HUST.id
+        })  
+
+
+        #create MO
+        strOrigin = 'mrp_multilevel_kitting Test Replenishment'
+        self.MW3_MO = self.env['mrp.production'].create({
+            'name': strOrigin,
+            'origin': strOrigin,
+            'product_tmpl_id': self.MW3.product_tmpl_id.id,
+            'product_id': self.MW3.id,
+            'product_qty': 1.0,
+            'location_src_id': self.HUST.id,
+            'location_dest_id': self.HUST.id,
+            'bom_id': self.MW3_BOM.id,
+            'product_uom_id': self.ref('uom.product_uom_unit')
+        })              
+
+        self.MW3_MO._onchange_move_raw()
+        self.MW3_MO.action_confirm()
+
+        myMoves = self.env['stock.move'].search([('origin', '=', strOrigin)])
+        move1 = myMoves.search([('product_id', '=', self.CompA.id)])
+        move2 = myMoves.search([('product_id', '=', self.CompB.id)])
+
+        self.assertEqual(len(move1), 1, msg='Expected move of CompA not found')
+        self.assertEqual(len(move2), 1, msg='Expected move of CompB not found')  
+
+        self.assertEqual(move1.location_id.id, testBin1.id, msg='CompA not drawng from existing location') 
+        self.assertEqual(move2.location_id.id, self.HUST.id, msg='CompB not from source')  
+
+    def test_diffbinsotherblank(self):
+        "Make sure multilevel_kitting off creates the need in-situ as normal"
+
+        self.warehouse.manu_type_id.multilevel_kitting = True
+        self.MW3_BOM.multilevel_kitting = True
+        self.compA_BOM_line.multilevel_kitting_name = ''
+        self.compB_BOM_line.multilevel_kitting_name = "TestBin2"
+
+        testBin1 = self.Location.create({
+            'name': 'TestBin1',
+            'location_id': self.HUST.id
+        })  
+
+
+        #create MO
+        strOrigin = 'mrp_multilevel_kitting Test Replenishment'
+        self.MW3_MO = self.env['mrp.production'].create({
+            'name': strOrigin,
+            'origin': strOrigin,
+            'product_tmpl_id': self.MW3.product_tmpl_id.id,
+            'product_id': self.MW3.id,
+            'product_qty': 1.0,
+            'location_src_id': self.HUST.id,
+            'location_dest_id': self.HUST.id,
+            'bom_id': self.MW3_BOM.id,
+            'product_uom_id': self.ref('uom.product_uom_unit')
+        })              
+
+        self.MW3_MO._onchange_move_raw()
+        self.MW3_MO.action_confirm()
+
+        myMoves = self.env['stock.move'].search([('origin', '=', strOrigin)])
+        move1 = myMoves.search([('product_id', '=', self.CompA.id)])
+        move2 = myMoves.search([('product_id', '=', self.CompB.id)])
+
+        self.assertEqual(len(move1), 1, msg='Expected move of CompA not found')
+        self.assertEqual(len(move2), 1, msg='Expected move of CompB not found')  
+
+        self.assertEqual(move1.location_id.id, self.HUST.id, msg='CompA not drawng from source') 
+        
+        self.assertEqual(move2.location_id.name, "TestBin2", msg='CompB location incorrect name')                      
+        self.assertEqual(move2.location_id.location_id.id, self.HUST.id, msg='CompB not drawn from child of source')              
