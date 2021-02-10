61f4d78ebbd9b09002fc5c130a03aa249601e9dd
Pete Seem <p.seem@durhammagnetooptics.com>
2021-02-10 10:26:04 +0000

    scaffolding

---
+++ dmo_accounting/README.rst
@@ -0,0 +1,12 @@
+==============================
+DMO Accounting
+==============================
+
+Various tweaks to Odoo accounting to better match DMO's work flow.
+
+Customer Invoice List View: Tax Excluded, Tax, Total, and Amount Due are also shown in local currency
+
+
+==================
+Known Limitations
+==================
---
+++ dmo_accounting/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2017 Graeme Gellatly
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'DMO Accounting',
+    'summary': 'Various tweaks to Odoo accounting to better match DMO work flow',
+    'version': '13.0.1.0.0',
+    'development_status': 'caveat emptor',
+    'category': 'Accounting',
+    'website': 'http://www.dmoltd.co.uk',
+    'author': 'Durham Magneto Optics Ltd',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account_move_views.xml',
+    ],
+    'demo': [
+        
+    ]
+}
