ac01a3c3bc448c2e312243b2ded5b5a6ab0b0b4f
Pete Seem <p.seem@durhammagnetooptics.com>
2021-03-18 21:03:30 +0000

    Cleaned up calcuation of amount Set "amount" currency to match journal currency

--- dmo_accounting/models/account_payment.py
+++ dmo_accounting/models/account_payment.py
@@ -5,12 +5,13 @@ class payment_register(models.TransientModel):
     _inherit = "account.payment.register"
     
     live_amount = fields.Monetary(string='Amount', compute="_compute_live_amount", readonly=True)
-    currency_id = fields.Many2one('res.currency', string='Currency', required=True, readonly=True, default=lambda self: self.env.company.currency_id)
+    currency_id = fields.Many2one('res.currency', compute ="_compute_live_amount", string='Currency', required=True, readonly=True, default=lambda self: self.env.company.currency_id)
 
     @api.depends('journal_id', 'invoice_ids', 'payment_date')
     def _compute_live_amount(self):
-        grouped = defaultdict(lambda: self.env["account.move"])
+        self.currency_id = self.journal_id.currency_id
+        invoices = self.env["account.move"]
         for inv in self.invoice_ids:
-            grouped['all'] += inv
-        invoices = grouped['all']
+            invoices += inv
         self.live_amount = abs(self.env['account.payment']._compute_payment_amount(invoices, invoices[0].currency_id, self.journal_id, self.payment_date))
+        
