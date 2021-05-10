786810209eb8ceb0013cd3a3b4c7afb5b06384d5
Pete Seem <p.seem@durhammagnetooptics.com>
2021-05-10 12:34:09 +0100

    Stock_locations_savespace honours removel Savespace follows the FIFO/LIFO removal strategy for >1 location.

--- stock_location_savespace/models/stock_location.py
+++ stock_location_savespace/models/stock_location.py
@@ -6,14 +6,23 @@ class Location(models.Model):
     putaway_savespace = fields.Boolean('Reuse bins', default=False, help="If no putaway rule is found, use existing stock location as the putaway strategy")
 
     def _get_putaway_strategy(self, product):
-        ''' Returns the location where the product has to be put, if any compliant putaway strategy is found. Otherwise returns None.'''
         res = super()._get_putaway_strategy(product)
         if not res and self.putaway_savespace:
             local_product = product.with_context(location=self.id)
-            if local_product.qty_available > 0.0:
+            if local_product.qty_available > 0.0:        
+
+                #if there's more than one location, choose using the same logic that we'd use for what stock to draw
+                removal_strategy = self.env['stock.quant']._get_removal_strategy(product, self)
+                if removal_strategy == 'fifo':
+                    sort_order = 'in_date'
+                elif removal_strategy == 'lifo':
+                    sort_order = 'in_date desc'
+                else:
+                    raise UserError(_('Removal strategy %s not implemented in stock_location_savespace.') % (removal_strategy,))
+
                 domain_quant, dummy, dummy = local_product._get_domain_locations()
                 domain_quant += [('product_id', '=', local_product.id), ('quantity', '>', 0.0)]                
-                quants = local_product.stock_quant_ids.search(domain_quant, order='in_date')
+                quants = local_product.stock_quant_ids.search(domain_quant, order=sort_order)
                 if quants:
-                    return quants[0].location_id
-        return res
+                    return quants[0].location_id  
+        return res   
