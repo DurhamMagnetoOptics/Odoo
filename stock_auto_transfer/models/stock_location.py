88164207c00ae9ade32ee0521e4332184783b609
Pete Seem <p.seem@durhammagnetooptics.com>
2021-05-19 21:24:08 +0100

    Changing PC

--- stock_auto_transfer/models/stock_location.py
+++ stock_auto_transfer/models/stock_location.py
@@ -1,4 +1,5 @@
 from odoo import api, models, fields
+from odoo.tools.misc import clean_context
 
 class Location(models.Model):
     _inherit = 'stock.location'
@@ -17,8 +18,24 @@ class Location(models.Model):
         self._auto_fulfill()
 
     def _auto_fulfill(self):
-        #TODO: generate procurement for -forecast_qty in me for each stock in self _only_ (not in children of self) with forecast_qty < 0
-        return {}       
+        if self.auto_fulfill:
+            #TODO: totally untested!
+            shortfall_products = self.env['product.product'].with_context({},location=self.id, compute_child=False).search([('virtual_available','<',0.0)])
+            procs = []
+            for prod in shortfall_products:
+                new_proc = self.env['procurement.group'].Procurement(
+                            prod,
+                            -prod.virtual_available,
+                            prod.uom_id,
+                            self,
+                            "Auto Replenishment",
+                            "Auto Replenishment",
+                            self.company_id,
+                            {}
+                        )
+                procs.append(new_proc)
+            if procs:
+                self.env['procurement.group'].with_context(clean_context(self.env.context)).run([procs])
 
     def run_auto_empty(self):
         self._auto_empty(None)
@@ -26,30 +43,22 @@ class Location(models.Model):
 
     def _auto_empty(self, target_product_ids):
         if self.auto_empty and self.auto_empty_target_id and self.auto_empty_operation_id:
-            #stockkidloc = self.env['product.product'].with_context({'location': self.location_id.id, 'compute_child': False}).search([('virtual_available','>',0.0)])
+            #TODO: totally untested!
+            excess_products = self.env['product.product'].with_context({},location=self.id, compute_child=False).search([('virtual_available','>',0.0)])
+            relevant_excess_products = (excess_products & target_product_ids) if target_product_ids else excess_products
 
-            #stockloc = self.env['product.product'].with_context({'location': self.location_id.id, 'compute_child': True}).search([('virtual_available','>',0.0)])
-            myloc = self.env['product.product'].with_context({},location=self.id, compute_child=False).search([('virtual_available','>',0.0)])
-            stockkidloc = self.env['product.product'].with_context({},location=self.location_id.id, compute_child=False).search([('virtual_available','>',0.0)])
-            stockloc = self.env['product.product'].with_context({},location=self.location_id.id, compute_child=True).search([('virtual_available','>',0.0)])
-            #Gather all target_product_ids for which forecast_qty > 0
-            #virtual_available
-
-            #contxt: location, compute_child
-            #new_picking = self.env['stock.picking'].create({
-            #    'location_id': self.id,
-            #    'location_dest_id': self.auto_empty_target_id.id,
-            #    'picking_type_id': self.auto_empty_operation_id.id,
-            #})
-            #new_move = self.env['stock.move'].create({
-            #    'name': 'test_link_assign_7',
-            #    'location_id': self.id,
-            #    'location_dest_id': self.auto_empty_target_id.id,
-            #    'product_id': self.product.id,
-            #    'product_uom_qty': 1.0,
-            #    'picking_id': new_picking.id,
-            #})            
-            #TODO: Generate (and assign!) auto_empty_operation_id's for forecast_qty from me to auto_empty_target_id for product_id (or all products) in self _only_ (not in children of self) with forecast_qty > 0
+            #TODO: Can I create just moves and let Odoo automatically build up the picking??
+            for prod in relevant_excess_products:
+                new_move = self.env['stock.move'].create({
+                    'location_id': self.id,
+                    'location_dest_id': self.auto_empty_target_id.id,
+                    'product_id': prod.id,
+                    'product_uom': prod.uom_id.id,
+                    'product_uom_qty': prod.virtual_available,
+                    'picking_type_id': self.auto_empty_operation_id.id
+                })  
+                new_move._action_confirm()
+                new_move._action_assign()
             pass
 
 
