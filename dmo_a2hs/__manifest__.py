b187cd2e4cf81cced0f510ff8c79282a9e773d5d
Pete Seem <p.seem@durhammagnetooptics.com>
2024-05-22 10:21:47 +0100

    Removed custom modules

--- dmo_a2hs/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-    "name": "DMO A2HS",
-    "version": "16.0.0.0.0",
-    "license": "OPL-1",
-    "author": "DMO Ltd",
-    "website": "https://www.durhammagnetooptics.com",
-    "summary": "Emtpy placeholder",
-}
