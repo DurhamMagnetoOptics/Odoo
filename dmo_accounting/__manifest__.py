9da126a2dbd0762c19ab23cee87bb934bdc82a78
Pete Seem <p.seem@durhammagnetooptics.com>
2024-05-22 10:21:47 +0100

    Removed custom modules

--- dmo_accounting/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-    'name': 'DMO Accounting',
-    "version": "16.0.0.0.0",
-    "license": "OPL-1",
-    "author": "DMO Ltd",
-    "website": "https://www.durhammagnetooptics.com",
-    "summary": "Emtpy placeholder",
-}
